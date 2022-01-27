387d2c5fcec6dc816b572d0a34cf69d8cfb52a1b
Munpun <bastianet@uia.no>
2022-01-27 11:04:15 +0100

    Legger til alle go-filene for modul 1

---
+++ myquote.go
@@ -0,0 +1,18 @@
+package myquote
+
+import "rsc.io/quote"
+
+func MittGlass() string {
+	return quote.Glass();
+}
+func MittGo() string {
+        return quote.Go()
+}
+func MittHello() string {
+        return quote.Hello()
+}
+func MittOpt() string {
+        return quote.Opt()
+}
+
+
